940bbacf2a34e6672564c4cf09da51a7ee48c734
DevAI Mastery <80696420+DevAIMastery@users.noreply.github.com>
2026-05-25 20:12:07 +0530

    Aadhaar form submit handler to Hindi

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.getElementById("aadharForm").addEventListener("submit", function (e) {
     ${data.relationType}: ${data.spouseHindi},<br>
     ग्राम -${data.villageHindi}, पोस्ट-${data.postHindi},<br>
     थाना–${data.psHindi}, ${data.villageHindi},<br>
-    ${data.district}, ${data.state}, ${data.pincode}
+    ${data.districtHindi}, ${data.stateHindi}, ${data.pincode}
   `;
 
   // English address
