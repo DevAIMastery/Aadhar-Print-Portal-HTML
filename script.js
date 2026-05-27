2186f1d5c56bcfedd4149ae40cf4cd83de829d65
DevAI Mastery <80696420+DevAIMastery@users.noreply.github.com>
2026-05-27 18:03:25 +0530

    Refactor Aadhaar form submit handler

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.getElementById("aadharForm").addEventListener("submit", function (e) {
     ${data.relationType}: ${data.spouseHindi},<br>
     ग्राम -${data.villageHindi}, पोस्ट-${data.postHindi},<br>
     थाना–${data.psHindi}, ${data.villageHindi},<br>
-    ${data.districtHi}, ${data.stateHi}, ${data.pincode}
+    ${data.districtHindi}, ${data.stateHindi}, ${data.pincode}
   `;
 
   // English address
